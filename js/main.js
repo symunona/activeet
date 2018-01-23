58d1e9a550d2c95912bbd5885851b0356de863bf
symunona <symunona@gmail.com>
2018-01-23 18:11:20 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,186 @@
+
+var ViewModel = function(){
+    
+    var self = window.app = this;
+
+    var timeToGuess = 60; // Seconds
+    var timeToWaitTillCountownStarts = 1000;
+
+    self.videoParams = '?autoplay=1&showinfo=0&rel=0&controls=0';    
+
+    self.currentVideoIndex = ko.observable(0);
+    // randomize(videos);
+    self.videos = ko.observable(videos);
+    self.currentVideo = ko.observable();
+    self.timerText = ko.observable();
+    self.timerStartDate = new Date();
+    self.error = ko.observable();
+    self.showResults = ko.observable();
+    self.showNext = ko.observable();
+    self.showReaction = ko.observable();
+    self.badMarkers = ['img/bad1.png','img/bad2.png','img/bad3.png']
+    
+    
+    self.guess = function(userGuess){
+        userGuess.guessed(true);
+        
+        if (userGuess.correct){            
+            self.playHappy();
+            self.success();      
+        }
+        else{
+            var guessedNumberFar = self.currentVideo()
+                .optionButtons.filter(function(button){
+                    return button.guessed();
+                }).length;
+            if (self.currentVideo().optionButtons.length-guessedNumberFar < 2){
+                self.fail();
+            }
+            self.playSad(function(){            
+                self.showReaction(false);        
+                self.startVideo();            
+            });
+        }
+    }
+
+    self.playSad = function(callback){        
+        var index = Math.floor(Math.random()*reactions.sad.length);
+        var reactionUrl = reactions.sad[index];        
+        self.showReaction(reactionUrl);
+        self.pauseVideo();
+        self.playReactionVideo(callback);        
+    }
+    self.playHappy = function(){
+        var index = Math.floor(Math.random()*reactions.happy.length);
+        var reactionUrl = reactions.happy[index];        
+        self.showReaction(reactionUrl);
+        self.pauseVideo();
+        self.playReactionVideo(function(){
+            self.showReaction(false);            
+            self.nextVideo();
+        });      
+    }
+
+    self.nextVideo = function(){
+        self.currentVideoIndex(self.currentVideoIndex()+1);
+        self.showReaction(false);
+        // Loop through videos
+        if (videos.length === self.currentVideoIndex()){
+            randomize(videos);
+            self.videos(videos);
+            self.currentVideoIndex(0);
+        }
+        self.loadVideo(self.currentVideoIndex());
+    }
+
+    self.loadVideo = function(index){
+        var video = videos[index];
+        video.optionButtons = [];
+        video.options.map(function(o, i){
+            video.optionButtons.push({
+                text: o,
+                guessed: ko.observable(false),
+                correct: video.correct === o,
+                index: i
+            });
+        });
+        randomize(video.optionButtons)
+        video.type = video.url.indexOf('https://www.youtube.com/') === 0?'youtube':'local';
+        
+        if (!video.optionButtons.filter(function(e){return e.correct}).length){
+            self.error('Nincs jó válasz. Nézz bele a videos.js fájlba!')
+            return;
+        }
+
+        randomize(self.buttons);
+        randomize(self.badMarkers);
+
+        self.showResults(false);
+        self.showNext(false);        
+
+        self.stopTimer();
+        
+        self.currentTime = video.time || timeToGuess
+        self.timerText(self.currentTime);
+
+        setTimeout(function(){
+            self.startVideo()
+            self.startTimer();
+        }, timeToWaitTillCountownStarts);
+        
+        self.currentVideo(video);
+    }
+
+    self.success = function(){
+        self.stopTimer();
+        setTimeout(function(){
+            self.timerText('Jó megfejtés!');                      
+            self.showNext(true);            
+        },0)
+    }
+    self.fail = function(){
+        self.stopTimer();
+        self.showResults(true);
+        self.playSad(function(){                    
+            self.showReaction(false);
+            self.nextVideo();                                
+        });
+    }
+    
+    self.stopTimer = function(){ self.timerRunning = false; }
+    self.startTimer = function(){ self.timerRunning = true; }
+
+    self.startVideo = function(){
+        var video = document.getElementById('davideo');
+        if (!video.ended){
+            video.play();
+        }        
+        self.timerRunning = true;
+    }
+    self.pauseVideo = function(){
+        var video = document.getElementById('davideo');
+        video.pause();
+        self.timerRunning = false;
+    }
+
+    self.playReactionVideo = function(callback){
+        var video = document.getElementById('reaction_video');
+        video.play();     
+        video.onended = callback;   
+    }
+
+    self.buttons = [];
+    for (var i=1; i<15; i++){
+        self.buttons.push('img/button'+i+'.png');
+    }
+
+    function randomize(array){
+        array.sort(function(){
+            return Math.random()>0.5?1:-1;
+        });
+    }
+
+    setInterval(function(){
+        if (self.timerRunning){
+            if (self.currentTime >= 0){                
+                self.timerText(self.currentTime);
+                self.currentTime = self.currentTime - 1;
+            }
+            else {
+                if (!self.showNext() && !self.showResults()){
+                    self.fail();                                        
+                }            
+                self.timerText('Lejárt az idő');                
+                self.timerRunning = false;
+            }            
+        }    
+    },1000)
+
+    self.loadVideo(0);
+
+    window.onerror = function(e){
+        self.error(e.message);
+    };    
+}
+
+ko.applyBindings(new ViewModel());
---
+++ videos.js
@@ -0,0 +1,150 @@
+var videos = [
+    {
+        "url": "videos/mutogatas_01_bujocska.mp4",
+        "options": ["bújocska", "kincskereső", "elveszett"],
+        "correct": "bújocska",
+        "time": 25
+    },
+    {
+        "url": "videos/mutogatas_02_dama.mp4",
+        "options": ["dáma", "vajaskenyér", "magasélet"],
+        "correct": "dáma",
+        "time": 25
+    },
+    {
+        "url": "videos/mutogatas_03_homokora.mp4",
+        "options": ["homokóra", "péklapát", "palacsintázó"],
+        "correct": "homokóra",
+        "time": 30
+    },
+    {
+        "url": "videos/mutogatas_04_verseny.mp4",
+        "options": ["futás", "csúfolódás", "verseny"],
+        "correct": "verseny",
+        "time": 15
+    },
+    {
+        "url": "videos/mutogatas_05_sakkmatt.mp4",
+        "options": ["sakk-matt", "Szécheny fürdő", "játék határok nélkül"],
+        "correct": "sakk-matt",
+        "time": 25
+    },
+    {
+        "url": "videos/mutogatas_06_lolepes.mp4",
+        "options": ["lólépés", "látens", "létra"],
+        "correct": "lólépés",
+        "time": 15
+    },
+    {
+        "url": "videos/mutogatas_07_oroszrulett.mp4",
+        "options": ["oroszrulett", "pisztoly", "optimizmus"],
+        "correct": "oroszrulett",
+        "time": 20
+    },
+    {
+        "url": "videos/mutogatas_08_szabaduloszoba.mp4",
+        "options": ["szabadulószoba", "szenvedéstörténet", "kínzókamra"],
+        "correct": "szabadulószoba",
+        "time": 30
+    },
+    {
+        "url": "videos/mutogatas_09_twister.mp4",
+        "options": ["twister", "fétis klub", "építkezés"],
+        "correct": "twister",
+        "time": 30
+    },
+    {
+        "url": "videos/mutogatas_10_pokerarc.mp4",
+        "options": ["pókerarc", "robot", "koncentráció"],
+        "correct": "pókerarc",
+        "time": 20
+    },
+    {
+        "url": "videos/mutogatas_11_parnacsata.mp4",
+        "options": ["párnacsata", "pizsiparty", "káosz"],
+        "correct": "párnacsata",
+        "time": 30
+    },
+    {
+        "url": "videos/mutogatas_12_kinevetavegen.mp4",
+        "options": ["kinevetavégén", "önértékelés", "hamis tanúzás"],
+        "correct": "kinevetavégén",
+        "time": 25
+    },
+    
+    {
+        "url": "videos/rajz_01_pokerarc.mp4",
+        "options": ["pókerarc", "hamiskártyás", "kártyatrükk"],
+        "correct": "pókerarc",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_02_kibic.mp4",
+        "options": ["kibic", "póker asztal", "díler"],
+        "correct": "kibic",
+        "time": 60
+    },
+    {
+        "url": "videos/rajz_03_lolepes.mp4",
+        "options": ["lólépés", "ügetés", "szamárfüles"],
+        "correct": "lólépés",
+        "time": 60
+    },
+    {
+        "url": "videos/rajz_04_libajatek.mp4",
+        "options": ["libajáték", "kacsamell", "madárlátta"],
+        "correct": "libajáték",
+        "time": 60
+    },
+    {
+        "url": "videos/rajz_05_bujocska.mp4",
+        "options": ["bújócska", "mikrofonpróba", "mérnök"],
+        "correct": "bújócska",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_06_kinevetavegen.mp4",
+        "options": ["kinevetavégén", "hétfő", "extasy"],
+        "correct": "kinevetavégén",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_07_szerencsekartya.mp4",
+        "options": ["szerencsekártya", "növénytan", "magyar kártya"],
+        "correct": "szerencsekártya",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_08_oroszrulett.mp4",
+        "options": ["oroszrulett", "összeesküvéselmélet", "casino"],
+        "correct": "oroszrulett",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_09_sakkmatt.mp4",
+        "options": ["sakk-matt", "túlsúly", "nyugdíjas klub"],
+        "correct": "sakk-matt",
+        "time": 50
+    },
+    {
+        "url": "videos/rajz_10_hamiskartyas.mp4",
+        "options": ["hamiskártyás", "póker", "kerekasztal"],
+        "correct": "hamiskártyás",
+        "time": 50
+    },
+    
+  
+]
+
+var reactions = {
+    happy: [
+        "videos/reactions/mutogatas__orom_01.mp4",
+        "videos/reactions/mutogatas__orom_03.mp4",
+        "videos/reactions/mutogatas__orom_04.mp4"
+    ],
+    sad: [
+        "videos/reactions/mutogatas__szomoru_01.mp4",
+        "videos/reactions/mutogatas__szomoru_02.mp4",
+        "videos/reactions/mutogatas__szomoru_03.mp4"
+    ]
+}
